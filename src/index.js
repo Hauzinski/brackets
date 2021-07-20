67d28a84672e4855de9321c085108d566820f3de
Vitali Hauzinski <hauzinski@gmail.com>
2021-07-20 11:34:58 +0300

    feat: add algorithm code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let array = str.split("");
+  let stack = 0;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    for (let e = 0; e < array.length; e++) {
+      if (array[e] === bracketsConfig[i][0]) {
+        stack += 1;
+      } else if (array[e] === bracketsConfig[i][1]) {
+        stack -= 1;
+      }
+      if (stack < 0) {
+        return false;
+      }
+    }
+    stack = 0;
+  }
+  return true;
+};
